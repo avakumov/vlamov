34fba12fab3a67d5083794787c056a54bb749abb
Avakumov Vladimir <avakdev@gmail.com>
2020-10-10 11:06:48 +0400

    added next-auth

--- components/top-navigation.js
+++ components/top-navigation.js
@@ -1,45 +1,67 @@
 import { CheckCircleOutlined, MailOutlined } from '@ant-design/icons'
 import { Menu } from 'antd'
+import { signin, signout, useSession } from 'next-auth/client'
 import Link from 'next/link'
-import React from 'react'
+import React, { useState } from 'react'
 
-class TopNavigation extends React.Component {
-    state = {
-        current: 'mail'
-    }
+const TopNavigation = () => {
+    const [current, setCurrent] = useState('mail')
+    const [session, loading] = useSession()
 
-    handleClick = (e) => {
+    const handleClick = (e) => {
         console.log('click ', e)
         this.setState({ current: e.key })
     }
 
-    render() {
-        const { current } = this.state
-        return (
-            <Menu onClick={this.handleClick} selectedKeys={[current]} mode="horizontal">
-                <Menu.Item key="home">
-                    <Link href="/">
-                        <img
-                            src="/images/profile.jpg"
-                            alt="home"
-                            className="top-navigation__home"
-                        />
-                    </Link>
-                </Menu.Item>
-                <Menu.Item key="contacts" icon={<MailOutlined />}>
-                    <Link href="/contacts">
-                        <a>Contacts</a>
-                    </Link>
-                </Menu.Item>
+    return (
+        <Menu onClick={handleClick} selectedKeys={[current]} mode="horizontal">
+            <Menu.Item key="home">
+                <Link href="/">
+                    <img src="/images/profile.jpg" alt="home" className="top-navigation__home" />
+                </Link>
+            </Menu.Item>
+            <Menu.Item key="contacts" icon={<MailOutlined />}>
+                <Link href="/contacts">
+                    <a>Contacts</a>
+                </Link>
+            </Menu.Item>
 
-                <Menu.Item key="tasks" icon={<CheckCircleOutlined />}>
-                    <Link href="/tasks">
-                        <a>Tasks</a>
-                    </Link>
-                </Menu.Item>
-            </Menu>
-        )
-    }
+            <Menu.Item key="tasks" icon={<CheckCircleOutlined />}>
+                <Link href="/tasks">
+                    <a>Tasks</a>
+                </Link>
+            </Menu.Item>
+            {!session && (
+                <>
+                    <span>Not signed in</span>
+                    <a
+                        href={`/api/auth/signin`}
+                        onClick={(e) => {
+                            e.preventDefault()
+                            signin()
+                        }}>
+                        <button>Sign in</button>
+                    </a>
+                </>
+            )}
+            {session && (
+                <>
+                    <span style={{ backgroundImage: `url(${session.user.image})` }} />
+                    <span>
+                        <strong>{session.user.email}</strong>
+                    </span>
+                    <a
+                        href={`/api/auth/signout`}
+                        onClick={(e) => {
+                            e.preventDefault()
+                            signout()
+                        }}>
+                        <button>Sign out</button>
+                    </a>
+                </>
+            )}
+        </Menu>
+    )
 }
 
 export default TopNavigation
--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,13 @@
 import '../styles/global.css'
 import 'antd/dist/antd.css'
 
+import { Provider } from 'next-auth/client'
+
 export default function App({ Component, pageProps }) {
-    return <Component {...pageProps} />
+    const { session } = pageProps
+    return (
+        <Provider session={session}>
+            <Component {...pageProps} />
+        </Provider>
+    )
 }
